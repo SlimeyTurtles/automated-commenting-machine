fdc00dc329b1a9b5d91f370763133f034fbd9050
Avinh Huynh <avinhahuynh@gmail.com>
2024-01-20 11:47:07 -0800

    refactor(config): add new fields `git_model_name`, `img_model_name`, `slides_prompt`, and `img_prompt` to Config struct, and update related functions to use the new fields.jj

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 mod config;
 mod git;
+mod img;
 use anyhow::{Context, Result};
+use config::Config;
 use dirs::home_dir;
 use reqwest::Client;
 use std::time::Duration;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let config_file = home_dir()
-        .context("Failed to retrieve config directory.")?
-        .join(".acm/config.toml");
-
-    let config = config::load_config(&config_file).await?;
-
+async fn git_commit(config: Config) -> Result<()> {
     git::git_checks().await?;
 
     let diffs = git::git_diff().await?;
@@ -24,6 +19,33 @@ async fn main() -> Result<()> {
     let commit_message = git::generate_commit_message(&http_client, &config, &diffs).await?;
     let commit_message = git::edit_commit_message(commit_message.trim())?;
     println!("{}", &git::git_commit(&commit_message).await?);
+
+    Ok(())
+}
+
+async fn image(config: Config) -> Result<()> {
+
+    let http_client = Client::builder()
+        .timeout(Duration::from_secs(config.request_timeout))
+        .build()?;
+
+    let img_url = img::generate_image_url(&http_client, &config, "small dog flying through clouds").await?;
+    println!("{}", img_url);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config_file = home_dir()
+        .context("Failed to retrieve config directory.")?
+        .join(".acm/config.toml");
+
+    let config = config::load_config(&config_file).await?;
+
+    git_commit(config).await?;
+
+    // image(config).await?;
 
     Ok(())
 }
