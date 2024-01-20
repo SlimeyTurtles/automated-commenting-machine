774464da140d3dce8e7325190762732a5024c780
Huy Dang <ghuydang@gmail.com>
2024-01-20 06:36:54 -0800

    refactor(git, main): add function to allow user to edit generated commit message before creating commit

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ async fn main() -> Result<()> {
         .build()?;
 
     let commit_message = git::generate_commit_message(&http_client, &config, &diffs).await?;
+    let commit_message = git::edit_commit_message(&commit_message)?;
     println!("Commit message: {}", commit_message,);
 
     Ok(())
