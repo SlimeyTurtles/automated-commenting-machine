0746fde02b08e3aefea1f4025c0ab5936bddcefe
Odera Nwosu <oderanwosu.j@gmail.com>
2024-01-20 01:30:11 -0800

    Added running read all files

--- src/handlers/comment.rs
+++ src/handlers/comment.rs
@@ -1,4 +1,41 @@
+use std::fs;
 
-pub fn execute_cmt(dir: &str, file_type: &str) {
-    println!("Executing command 2 with argument: {}, {}", dir, file_type);
+fn is_directory(file_path: &str) -> bool {
+    if let Ok(metadata) = fs::metadata(file_path) {
+        metadata.is_dir()
+    } else {
+        false
+    }
+}
+
+pub fn execute_cmt(dir: &str, req_file_type: &str) {
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                // Get the file name as a String
+
+                if let Some(file_dir) = entry.path().to_str() {
+                    if is_directory(file_dir) {
+                        execute_cmt(file_dir, req_file_type)
+                    } else {
+                        if let Some(file_name) = entry.file_name().to_str() {
+                            println!("Found file: {}", file_name);
+                            if let Some(extension) = entry.path().extension() {
+                                if let Some(extension_str) = extension.to_str() {
+                                    println!("Text after last dot: {}", extension_str);
+                                } else {
+                                    eprintln!("Failed to convert extension to string.");
+                                }
+                            } else {
+                                println!("No extension found in the file name.");
+                            }
+
+                            // let contents = fs::read_to_string(entry.path())
+                            //     .expect("Should have been able to read the file");
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
