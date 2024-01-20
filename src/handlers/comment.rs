65cef584508e2d4a26a3d957b719365c16f347bb
Odera Nwosu <oderanwosu.j@gmail.com>
2024-01-20 06:57:58 -0800

    set up for push

--- src/handlers/comment.rs
+++ src/handlers/comment.rs
@@ -8,30 +8,28 @@ fn is_directory(file_path: &str) -> bool {
     }
 }
 
+fn modify_file(dir: &str) {
+    match fs::read_to_string(dir) {
+        Ok(contents) => {
+            // Successfully read the file contents
+            println!("File contents:\n{}", contents);
+        }
+        Err(e) => {
+            // Handle the error if the file cannot be read
+            eprintln!("Error reading file: {}", e);
+        }
+    }
+}
+
 pub fn execute_cmt(dir: &str, req_file_type: &str) {
+    if !is_directory(dir) {
+        modify_file(dir);
+        return;
+    }
     if let Ok(entries) = fs::read_dir(dir) {
         for entry in entries.flatten() {
-
-            if is_directory(entry.path().to_str().unwrap_or(".")) {
-                execute_cmt(entry.path().to_str().unwrap_or("."), req_file_type);
-                continue;
-            }
-
-            if let Some(file_name) = entry.file_name().to_str() {
-                println!("Found file: {}", file_name);
-
-                if let Some(extension) = entry.path().extension() {
-                    if let Some(extension_str) = extension.to_str() {
-                        println!("Text after last dot: {}", extension_str);
-                    } else {
-                        eprintln!("Failed to convert extension to string.");
-                    }
-                } else {
-                    println!("No extension found in the file name.");
-                }
-            }
-
-        
+            execute_cmt(entry.path().to_str().unwrap_or("."), req_file_type);
+            continue;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,6 @@ use clap::{App, Arg, SubCommand};
 
 
 //Commands the client can request.
-
 fn main() {
-    let matches = App::new("ACM")
-        .version("1.0.0")
-        .author("SF Hacks @ CruzHacks ^-^")
-        .about("This command tool allows you to use GPT to modify your existing project and add comments")
-        .subcommand(
-            SubCommand::with_name("prs")
-                .about("Reads the files specified in the path and creates slide document ")
-                .arg(Arg::with_name("dir").required(true).help("The name of the directory comments are wanted on").default_value("./")).arg(Arg::with_name("type").help("The file type to add comments to")),
-        )
-        .subcommand(
-            SubCommand::with_name("gc")
-                .about("Adds a git commit")
-               
-        )
-        .get_matches();
-
-    match matches.subcommand(){
-        ("prs", Some(cmd)) => {
-            let dir = cmd.value_of("dir").unwrap();
-            let file_type = cmd.value_of("type").unwrap_or("all");
-            execute_cmt(dir
-                , file_type)
-            
-          
-        },_ => {
-            println!("No or unknown subcommand provided.");
-        }
-    }
+   
 }
